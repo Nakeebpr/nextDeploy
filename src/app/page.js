db0c1fd3937126824d9ea72a2667beae876cf761
Nakeebpr <88592866+Nakeebpr@users.noreply.github.com>
2023-09-13 21:07:46 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            Byyyy{' '}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
